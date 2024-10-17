9227065c0ed5f7b55d75f657f959c0377c1ebf50
zxckawory <kaworylox@gmail.com>
2024-10-17 14:43:31 +0300

    Delete Script-3.sql

--- Script-3.sql
+++
@@ -1,9 +0,0 @@
-select ("FirstName"|| ' ' ||"LastName") as "RunnerName", date_part('year', age(current_date, "DateOfBirth")) as "Age", "Gender", row_number() over (partition by e."EventId"order by "RaceTime") AS "Position", (("RaceTime" / 60 / 60)::text || 'h ' || (("RaceTime" / 60 % 60)::text || 'min ' ||  ("RaceTime" % 60)::text || 'sec')) as "RaceTime", "MarathonName", "EventName"  from "Marathon" m 
-join "Event" e on "e"."MarathonId" = "m"."MarathonId"
-join "RegistrationEvent" re on "re"."EventId" = "e"."EventId" 
-join "Registration" r on "r"."RegistrationId" = "re"."RegistrationId" 
-join "Runner" ru on "ru"."RunnerId" = "r"."RunnerId"
-join "User" u on "u"."Email" = "ru"."Email"
-join "Gender" g on "g"."Gender" = "ru"."GenderId"
-where "RaceTime" is not null and "RaceTime"!=0
-order by e."EventId", "Gender", "Position";
