ca0e3b11489795f704bf4f675ac06a2f3a43bb8f
zxckawory <kaworylox@gmail.com>
2024-10-17 14:13:21 +0300

    Delete Script-1.sql

--- Script-1.sql
+++
@@ -1,3 +0,0 @@
-select ("FirstName"|| ' ' ||"LastName") as "RunnerName", r."CountryCode"  from "Runner" r 
-join "Country" c on r."CountryCode"=c."CountryCode" 
-join "User" u on r."Email"=u."Email";
